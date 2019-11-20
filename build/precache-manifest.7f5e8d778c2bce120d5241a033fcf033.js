b4ccea2b1d7f72cc46385216e9e68341fb15a206
Brad Anderson <banderso@nerdery.com>
2019-11-20 10:07:01 -0500

    Quick and dirty deploy

---
+++ build/precache-manifest.7f5e8d778c2bce120d5241a033fcf033.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2308fdbaedcba9e11174c498fa616b3c",
+    "url": "/create-thunk/build/index.html"
+  },
+  {
+    "revision": "a9ab31619112fe672d7e",
+    "url": "/create-thunk/build/static/css/main.e893a164.chunk.css"
+  },
+  {
+    "revision": "dbde813a0e3c3df7316b",
+    "url": "/create-thunk/build/static/js/2.fb75d324.chunk.js"
+  },
+  {
+    "revision": "a9ab31619112fe672d7e",
+    "url": "/create-thunk/build/static/js/main.669cad6e.chunk.js"
+  },
+  {
+    "revision": "b69b10078778aa5d3c36",
+    "url": "/create-thunk/build/static/js/runtime~main.04c875b5.js"
+  }
+]);
